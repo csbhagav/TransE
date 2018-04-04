d3de9d30831b8ff69ca1cedf581d498f7567359a
chandrab <chandrab@allenai.org>
2018-04-03 20:57:48 -0700

    Read table changed to read csv

--- transe/dataset.py
+++ transe/dataset.py
@@ -29,13 +29,15 @@ class KnowledgeGraph:
         entity_dict_file = 'entity2id.txt'
         relation_dict_file = 'relation2id.txt'
         print('-----Loading entity dict-----')
-        entity_df = pd.read_table(os.path.join(self.data_dir, entity_dict_file), header=None)
+        entity_df = pd.read_csv(os.path.join(self.data_dir, entity_dict_file), header=None,
+                                sep='\t', keep_default_na=False)
         self.entity_dict = dict(zip(entity_df[0].astype(str), entity_df[1]))
         self.n_entity = len(self.entity_dict)
         self.entities = list(self.entity_dict.values())
         print('#entity: {}'.format(self.n_entity))
         print('-----Loading relation dict-----')
-        relation_df = pd.read_table(os.path.join(self.data_dir, relation_dict_file), header=None)
+        relation_df = pd.read_csv(os.path.join(self.data_dir, relation_dict_file), header=None,
+                                  sep='\t', keep_default_na=False)
         self.relation_dict = dict(zip(relation_df[0].astype(str), relation_df[1]))
         self.n_relation = len(self.relation_dict)
         print('#relation: {}'.format(self.n_relation))
@@ -45,21 +47,23 @@ class KnowledgeGraph:
         validation_file = 'valid.txt'
         test_file = 'test.txt'
         print('-----Loading training triples-----')
-        training_df = pd.read_table(os.path.join(self.data_dir, training_file), header=None)
+        training_df = pd.read_csv(os.path.join(self.data_dir, training_file), header=None,
+                                  sep='\t', keep_default_na=False)
         self.training_triples = list(zip([self.entity_dict[h] for h in training_df[0]],
                                          [self.entity_dict[t] for t in training_df[1]],
                                          [self.relation_dict[r] for r in training_df[2]]))
         self.n_training_triple = len(self.training_triples)
         print('#training triple: {}'.format(self.n_training_triple))
         print('-----Loading validation triples-----')
-        validation_df = pd.read_table(os.path.join(self.data_dir, validation_file), header=None)
+        validation_df = pd.read_csv(os.path.join(self.data_dir, validation_file), header=None,
+                                    sep='\t', keep_default_na=False)
         self.validation_triples = list(zip([self.entity_dict[h] for h in validation_df[0]],
                                            [self.entity_dict[t] for t in validation_df[1]],
                                            [self.relation_dict[r] for r in validation_df[2]]))
         self.n_validation_triple = len(self.validation_triples)
         print('#validation triple: {}'.format(self.n_validation_triple))
         print('-----Loading test triples------')
-        test_df = pd.read_table(os.path.join(self.data_dir, test_file), header=None)
+        test_df = pd.read_csv(os.path.join(self.data_dir, test_file), header=None, sep='\t', keep_default_na=False)
         self.test_triples = list(zip([self.entity_dict[h] for h in test_df[0]],
                                      [self.entity_dict[t] for t in test_df[1]],
                                      [self.relation_dict[r] for r in test_df[2]]))
